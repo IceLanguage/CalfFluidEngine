163a0391649cb7d6b14d03cbf86ab399c083ca5e
Icelanguage <845854051@qq.com>
2019-06-29 00:27:19 +0800

    使用雅克比方法求解线性方程组

--- src/FDMLinearSystem3.cpp
+++ src/FDMLinearSystem3.cpp
@@ -2,4 +2,53 @@
 
 using namespace CalfFluidEngine;
 
+bool CalfFluidEngine::FDMJacobiSolver3::Solve(FDMLinearSystem3 * system)
+{
+	_xTemp.Resize(system->x.Size());
+	_residual.Resize(system->x.Size());
 
+	unsigned int lastNumberOfIterations = _maxNumberOfIterations;
+
+	for (unsigned int iter = 0; iter < _maxNumberOfIterations; ++iter) {
+
+		relax(system->A, system->b, &system->x, &_xTemp);
+		_xTemp.Swap(system->x);
+
+		if (iter != 0 && iter % _residualCheckInterval == 0) {
+
+			Residual(system->A, system->x, system->b, &_residual);
+
+			if (std::sqrt(FDMLinearSystem3::Dot(_residual, _residual)) < _maxResidualTolerance)
+			{
+				lastNumberOfIterations = iter + 1;
+				break;
+			}
+		}
+	}
+	Residual(system->A, system->x, system->b, &_residual);
+
+	return std::sqrt(FDMLinearSystem3::Dot(_residual, _residual)) < _maxResidualTolerance;
+}
+
+void CalfFluidEngine::FDMJacobiSolver3::relax(
+	const Array3<FDMMatrixRow3>& R, 
+	const Array3<double>& b, 
+	Array3<double>* x_, 
+	Array3<double>* xTemp_)
+{
+	Vector3<size_t> size = R.Size();
+	Array3<double>& x = *x_;
+	Array3<double>& xTemp = *xTemp_;
+
+	R.ParallelForEach([&](size_t i, size_t j, size_t k) {
+		double r =
+			((i > 0) ? R(i - 1, j, k).right * x(i - 1, j, k) : 0.0) +
+			((i + 1 < size.x) ? R(i, j, k).right * x(i + 1, j, k) : 0.0) +
+			((j > 0) ? R(i, j - 1, k).up * x(i, j - 1, k) : 0.0) +
+			((j + 1 < size.y) ? R(i, j, k).up * x(i, j + 1, k) : 0.0) +
+			((k > 0) ? R(i, j, k - 1).front * x(i, j, k - 1) : 0.0) +
+			((k + 1 < size.z) ? R(i, j, k).front * x(i, j, k + 1) : 0.0);
+
+		xTemp(i, j, k) = (b(i, j, k) - r) / R(i, j, k).center;
+	});
+}
--- src/FDMLinearSystem3.h
+++ src/FDMLinearSystem3.h
@@ -12,11 +12,25 @@ namespace CalfFluidEngine {
 
 	struct FDMLinearSystem3
 	{
+	public:
 		//Ax = b
 		Array3<FDMMatrixRow3> A;
 		Array3<double> x, b;
+
+		static double Dot(const Array3<double>& a, const Array3<double>& b) {
+			Vector3<size_t> size = a.Size();
+
+			double result = 0.0;
+
+			a.ParallelForEach([&](size_t i, size_t j, size_t k) {
+				result += a(i, j, k) * b(i, j, k);
+			});
+
+			return result;
+		}
 	};
 
+	// res = m * v
 	inline void MVM(const Array3<FDMMatrixRow3>& matrix, const Array3<double>& vector, Array3<double>* result)
 	{
 		Vector3<size_t> size = matrix.Size();
@@ -32,6 +46,26 @@ namespace CalfFluidEngine {
 		});
 	}
 
+	// res = A - bx
+	inline void Residual(
+		const Array3<FDMMatrixRow3>& A, 
+		const Array3<double>& x, 
+		const Array3<double>& b, 
+		Array3<double>* result)
+	{
+		Vector3<size_t> size = A.Size();
+		A.ParallelForEach([&](size_t i, size_t j, size_t k) {
+			(*result)(i, j, k) =
+				b(i, j, k) - A(i, j, k).center * x(i, j, k) -
+				((i > 0) ? A(i - 1, j, k).right * x(i - 1, j, k) : 0.0) -
+				((i + 1 < size.x) ? A(i, j, k).right * x(i + 1, j, k) : 0.0) -
+				((j > 0) ? A(i, j - 1, k).up * x(i, j - 1, k) : 0.0) -
+				((j + 1 < size.y) ? A(i, j, k).up * x(i, j + 1, k) : 0.0) -
+				((k > 0) ? A(i, j, k - 1).front * x(i, j, k - 1) : 0.0) -
+				((k + 1 < size.z) ? A(i, j, k).front * x(i, j, k + 1) : 0.0);
+		});
+	}
+
 	class IFDMLinearSystemSolver3
 	{
 	public:
@@ -44,6 +78,25 @@ namespace CalfFluidEngine {
 		//**********************************************
 		virtual bool Solve(FDMLinearSystem3* system) = 0;
 
+	};
+
+	class FDMJacobiSolver3 final : public IFDMLinearSystemSolver3 {
+	public:
+		virtual bool Solve(FDMLinearSystem3* system) override;
+	private:
+
+		// formula in markdown: $ x^{(k+1)}_i  = \frac{1}{a_{ii}} \left(b_i -\sum_{j\ne i}a_{ij}x^{(k)}_j\right),\quad i=1,2,\ldots,n.$
+		void relax(
+			const Array3<FDMMatrixRow3>& A, 
+			const Array3<double>& b, 
+			Array3<double>* x,
+			Array3<double>* xTemp);
+
+		unsigned int _maxNumberOfIterations;
+		unsigned int _residualCheckInterval;
+		double _maxResidualTolerance;
+		Array3<double> _xTemp;
+		Array3<double> _residual;
 	};
 }
 #endif
